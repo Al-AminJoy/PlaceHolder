38bb9f4097cdc82434c1c2ba976dd16b1885a430
Al-AminJoy <alaminislam3555@gmail.com>
2022-03-25 03:15:47 +0600

    DataStore Added

---
+++ app/src/main/java/com/alamin/placeholder/utils/LocalDataStore.kt
@@ -0,0 +1,54 @@
+package com.alamin.placeholder.utils
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.map
+
+
+const val PREFERENCE_NAME = "data_store"
+
+class LocalDataStore(val context: Context ) {
+
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(PREFERENCE_NAME)
+
+    companion object PreferenceKeys{
+        val USER = stringPreferencesKey("user")
+        val ID = intPreferencesKey("user_id")
+        val NAME = stringPreferencesKey("user_name")
+    }
+
+    suspend fun storeUser(user: String){
+        context.dataStore.edit {
+            it[USER] = user;
+        }
+    }
+
+    suspend fun storeId(id:Int){
+        context.dataStore.edit {
+            it[ID] = id
+        }
+    }
+
+    suspend fun storeName(name: String){
+        context.dataStore.edit {
+            it[NAME] = name;
+        }
+    }
+
+    fun getUser() = context.dataStore.data.map {
+        it[USER]?:-1
+    }
+
+    fun getId() = context.dataStore.data.map {
+        it[ID]?:-1
+    }
+
+    fun getName() = context.dataStore.data.map {
+        it[NAME]?:-1
+    }
+}
--- app/src/main/java/com/alamin/placeholder/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/alamin/placeholder/view/fragment/LoginFragment.kt
@@ -7,20 +7,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.alamin.placeholder.R
 import com.alamin.placeholder.databinding.FragmentLoginBinding
+import com.alamin.placeholder.utils.LocalDataStore
 import com.alamin.placeholder.view_model.UserViewModel
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
+    private val TAG = "LoginFragment"
     lateinit var binding: FragmentLoginBinding;
     lateinit var userViewModel: UserViewModel;
+    lateinit var localDataStore: LocalDataStore;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        localDataStore = LocalDataStore(requireContext())
+
+        lifecycleScope.launchWhenCreated {
+            localDataStore.getName().collect {
+                if (it !=-1){
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment);
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,12 +47,18 @@ class LoginFragment : Fragment() {
         binding.userViewModel = userViewModel;
         binding.lifecycleOwner = this;
 
+
+
         binding.btnLogin.setOnClickListener {
             if (validateData(binding.txtUid.text)){
                 userViewModel.user.observe(viewLifecycleOwner, Observer {
                     if (it != null){
                         lifecycleScope.launch {
+                            Log.d(TAG, "USER "+it)
                             userViewModel.createUser(it);
+                        }
+                        lifecycleScope.launch {
+                            localDataStore.storeName(it.name)
                         }
                         findNavController().navigate(R.id.action_loginFragment_to_homeFragment);
                     }
